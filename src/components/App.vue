df09d92262f09787ab05b153ff1e8f0b961d45e9
larrysherrill <larry@version2software.com>
2021-07-15 13:12:52 -0600

    move App.vue to typescript

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,33 +2,36 @@
     <component v-bind:is="currentView" :readOnly="readOnly"></component>
 </template>
 
-<script>
+<script lang="ts">
     import timerComponent from './Timer.vue';
     import eventsComponent from './Events.vue';
     import printComponent from './Print.vue';
     import aboutComponent from './About.vue';
     import privacyComponent from './Privacy.vue';
+    import {defineComponent} from 'vue'
+    import {Component} from "@vue/runtime-core";
 
-    export default {
+    export default defineComponent({
         data() {
             return {
-                currentView: null
+                readOnly: false, // Used by privacy windows
+                currentView: null as Component
             };
         },
         mounted: function () {
-            window.api.register('page', data => {
-                if (data === 'about') {
+            window.api.register('page', (pageName:string) => {
+                if (pageName === 'about') {
                     this.currentView = aboutComponent
-                } else if (data === 'timer') {
+                } else if (pageName === 'timer') {
                     this.currentView = timerComponent
-                } else if (data === 'events') {
+                } else if (pageName === 'events') {
                     this.currentView = eventsComponent
-                } else if (data === 'print') {
+                } else if (pageName === 'print') {
                     this.currentView = printComponent
-                } else if (data === 'privacy') {
+                } else if (pageName === 'privacy') {
                     this.readOnly = false
                     this.currentView = privacyComponent
-                } else if (data === 'privacy-readonly') {
+                } else if (pageName === 'privacy-readonly') {
                     this.readOnly = true
                     this.currentView = privacyComponent
                 }
@@ -41,7 +44,7 @@
             printComponent,
             privacyComponent
         }
-    }
+    });
 </script>
 
 <style>
