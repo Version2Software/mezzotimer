133136921d7c97dbc1d579ef8976acd52d5f9905
larrysherrill <larry@version2software.com>
2021-08-13 12:43:26 -0600

    Convert App.vue to script setup.

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,8 +2,9 @@
     <component v-bind:is="currentView" :readOnly="readOnly"></component>
 </template>
 
-<script lang="ts">
-import {defineComponent, ref, onMounted} from 'vue'
+<script lang="ts" setup>
+
+import {ref, onMounted} from 'vue'
 import {Component} from "@vue/runtime-core";
 
 import timerComponent from './Timer.vue';
@@ -12,43 +13,29 @@ import printComponent from './Print.vue';
 import aboutComponent from './About.vue';
 import privacyComponent from './Privacy.vue';
 
-export default defineComponent({
-    setup() {
-        const readOnly = ref(false);  // Used by privacy windows
-        const currentView = ref(null as Component);
+const readOnly = ref(false);  // Used by privacy windows
+const currentView = ref(null as Component);
 
-        onMounted(() => {
-            window.api.register('page', (pageName: string) => {
-                if (pageName === 'about') {
-                    currentView.value = aboutComponent
-                } else if (pageName === 'timer') {
-                    currentView.value = timerComponent
-                } else if (pageName === 'events') {
-                    currentView.value = eventsComponent
-                } else if (pageName === 'print') {
-                    currentView.value = printComponent
-                } else if (pageName === 'privacy') {
-                    readOnly.value = false
-                    currentView.value = privacyComponent
-                } else if (pageName === 'privacy-readonly') {
-                    readOnly.value = true
-                    currentView.value = privacyComponent
-                }
-            })
-        })
-        return {
-            readOnly,
-            currentView
-        };
-    },
-    components: {
-        timerComponent,
-        aboutComponent,
-        eventsComponent,
-        printComponent,
-        privacyComponent
-    }
+onMounted(() => {
+    window.api.register('page', (pageName: string) => {
+        if (pageName === 'about') {
+            currentView.value = aboutComponent
+        } else if (pageName === 'timer') {
+            currentView.value = timerComponent
+        } else if (pageName === 'events') {
+            currentView.value = eventsComponent
+        } else if (pageName === 'print') {
+            currentView.value = printComponent
+        } else if (pageName === 'privacy') {
+            readOnly.value = false
+            currentView.value = privacyComponent
+        } else if (pageName === 'privacy-readonly') {
+            readOnly.value = true
+            currentView.value = privacyComponent
+        }
+    })
 });
+
 </script>
 
 <style>
